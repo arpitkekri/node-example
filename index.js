9012f9ab6ae3586b64ba32e126456bcbdef1ac86
arpitkekri <jainarpitkekri@gmail.com>
2020-07-28 03:30:44 +0530

    Simple Node Module

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-var rect = require('./rectangle');
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
-
-    if(l<=0 || b<=0) {
-        console.log("Rectangle dimensions should be greater than zero: l = " + l + ", and b = " + b);
-    }
-    else {
-        console.log("The area of the ractangle is " + rect.area(l,b));
-        console.log("The perimeter of the ractangle is " + rect.perimeter(l,b));
-    }
-}
-
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
-
 var msg = require('./logger.js');
 msg.logger('Hello World');
 
 var myLogModule = require('./log.js'); //.js is not a compulsory thing
 myLogModule.info('Node.js started');
+myLogModule.error("404 Not Found");
+myLogModule.warning(`This is a Warning`);
+
+var person = require('./data.js');
+console.log(person.firstName + ' ' + person.lastName);
+
+
