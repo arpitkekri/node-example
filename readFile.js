515ae164ea1903ac10b4361b99d11544dfdd6dea
arpitkekri <jainarpitkekri@gmail.com>
2020-07-28 12:53:32 +0530

    Node callback and error handling

--- readFile.js
+++ readFile.js
@@ -1,23 +1,22 @@
-// Without use of callback (simple program)
-// // if file is too big take some time
+/* 
+*Without use of callback (simple program)
+*if file is too big take some time 
+*/
 
 // var fs = require("fs");
-
 // console.log("Program Started");
 // var data = fs.readFileSync('input.txt');
-
 // console.log(data.toString());
 // console.log("Program Ended");
 
 
-// Use of callback
+// By using callback function
 var fs = require("fs");
-
 console.log("Program Started");
-fs.readFile("input.txt", function (err, data) {
-    if(err) 
-        return console.log(err);
-
+fs.readFile("input.txt", 
+function (err, data) {
+    if(err) return console.log(err); 
     console.log(data.toString());
-});
+}
+);
 console.log("Program Ended"); // executed while reading file then data will be printed
