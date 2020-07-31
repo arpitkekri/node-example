c594792c981f8926a24dd7da3cde741f9e010968
arpitkekri <jainarpitkekri@gmail.com>
2020-07-31 23:54:30 +0530

    updeted all commands as notes

--- Node Package Manager/npm.js
+++ Node Package Manager/npm.js
@@ -1,1 +1,21 @@
 console.log("This is tutorial for npm 1.0.0");
+/*
+npm --version or npm -v 
+node --version or node -v 
+npm init  //To initialize npm node package in this folder
+npm install or npm i //for installing package or old deleted package
+npm install slugiufy // make dependencies in package.json
+mnp install express
+npm uninstall express
+npm install slugify@1.3.5 // for install specific version
+1.3.5  ==> major.minor.patch // patch is bug fixes
+npm install nodemon --save-dev // gives functionality to restart your server automatically
+// --save-dev ==> makes nodemon to developer dependency only use for development not for production
+npm install nodemon --global // install globally for your entire project
+npm i browserify
+// in package.json file ^ means install exactly mentioned version
+// in package.json file ~ means install letest patch for current version
+// in package.json file > x.x.x means install letest version greater than mentiond version
+// if we delete npm modules then just run ==> npm i or nmp install for retrive all modules
+
+*/
