c2880f94b364f68d71abc38b193aabbb4b949b9a
arpitkekri <jainarpitkekri@gmail.com>
2020-07-31 20:38:29 +0530

    read and write file module

--- readFile.js
+++ readFile.js
@@ -2,7 +2,8 @@
 *Without use of callback (simple program)
 *if file is too big take some time 
 */
-
+// Synchronous or blocking
+// - line by line execution
 const fs = require("fs");
 console.log("Program Started");
 let data = fs.readFileSync('input.txt', "utf-8");
@@ -11,16 +12,16 @@ console.log(data.toString());
 console.log("Creating a new file...");
 fs.writeFileSync("newFile.txt", data);
 console.log("Program Ended");
-
-/*
+ 
+// Asynchronous or non-blocking
+// - line by line execution not guaranteed
+// - callbacks will fire
 // By using callback function
-const fs = require("fs");
-console.log("Program Started");
-fs.readFile("input.txt", 
+const newFs = require("fs");
+console.log("New Program Started");
+newFs.readFile("input.txt", "utf-8",
 function (err, data) {
     if(err) return console.log(err); 
     console.log(data.toString());
-}
-);
+});
 console.log("Program Ended"); // executed while reading file then data will be printed
-*/
