93e9ddb3bccd9f89da8cf35d768ffbace5ae1b77
arpitkekri <jainarpitkekri@gmail.com>
2020-08-01 17:07:51 +0530

    get and post request in express and show content in postman

--- Express/app.js
+++ Express/app.js
@@ -1,2 +1,27 @@
 // npm install express 
 // We do not have to do manual url parshing and write custom backend express will handle it
+
+const express = require("express");
+
+const app = express();
+const port = 80;
+app.get("/", (req, res)=>{
+    res.status(200).send("This is my first express app tutorial in learning phase");
+});
+
+app.get("/about", (req, res)=>{
+    res.send("This is about page of express app tutorial in learning phase");
+});
+
+app.post("/about", (req, res)=>{
+    res.send("This is post request about page of express app tutorial in learning phase");
+});
+
+app.get("/this", (req, res)=>{
+    res.setHeader('Content-Type', 'text/HTML');
+    res.status(400).send(`<h1>This page is not found on my website</h1>`);
+});
+
+app.listen(port, ()=>{
+    console.log(`This application started successfully on port ${port}`);
+});
