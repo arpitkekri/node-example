82b36ea4b58c40f610ca9d2bbc1b8952014815a2
arpitkekri <jainarpitkekri@gmail.com>
2020-08-02 01:20:28 +0530

    made a backend using pug and express

--- Express/app.js
+++ Express/app.js
@@ -1,40 +1,40 @@
 // npm install express 
 // We do not have to do manual url parshing and write custom backend express will handle it
-
+// npm install pug ==> to install pug
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 80;
 
-// For serving static file
-app.use('/static', express.static('static'));
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')); // For serving static file
+app.use(express.urlencoded())
 
-// Set the template engine as pug
-app.set('view engine','pug');
-// Set the view directory
-app.set('views', path.join(__dirname, 'views'));
-// Our pug demo endpoint
-app.get("/demo", (req,res)=>{
-    res.status(200).render('demo', { title: 'Hey Arpit', message: 'Hello there and thanks for telling me how to use pug' })
-});
+// PUG SPECIFIC STUFF
+app.set('view engine','pug'); // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')); // Set the view directory
 
+// ENDPOINTS
 app.get("/", (req, res)=>{
-    res.status(200).send("This is my first express app tutorial in learning phase");
+    const con = "This is the best content on the internet so far so use it wisely";
+    const params = {'title': 'CS go is the best game', "content": con}
+    res.status(200).render("index.pug", params);
 });
 
-app.get("/about", (req, res)=>{
-    res.send("This is about page of express app tutorial in learning phase");
+app.post("/", (req, res) => {
+    name = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    address = req.body.address
+    more = req.body.more
+    let outputToWrite = `The name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him/her: ${more}`
+    fs.writeFileSync('output.txt', outputToWrite)
+    const params = {'message': 'Your form has been submitted succesfully'}
+    res.status(200).render("index.pug", params);
 });
 
-app.post("/about", (req, res)=>{
-    res.send("This is post request about page of express app tutorial in learning phase");
-});
-
-app.get("/this", (req, res)=>{
-    res.setHeader('Content-Type', 'text/HTML');
-    res.status(400).send(`<h1>This page is not found on my website</h1>`);
-});
-
+// START THE SERVER
 app.listen(port, ()=>{
     console.log(`This application started successfully on port ${port}`);
 });
